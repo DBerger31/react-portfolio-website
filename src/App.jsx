1a026794414a4ca8d4bb1ba0e31978410b9c3d2c
Daniel Berger <DanielBergerNY@gmail.com>
2022-10-13 00:45:03 -0400

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from "./components/header/Header"
+import Nav from "./components/nav/Nav"
+import About from "./components/about/About"
+import Experience from "./components/experience/Experience"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+    </>
   )
 }
 
